fastred/AHKBendableView

commit c9f3b1b94e3894fc13f1034a2412e85923173a55

Rename all occurrences of Bending to Bendable

Classes/BendableView.swift
@@ -1,6 +1,6 @@
 //
-//  BendingView.swift
-//  AHKBendingView
+//  BendableView.swift
+//  AHKBendableView
 //
 //  Created by Arkadiusz on 26-06-14.
 //  Copyright (c) 2014 Arkadiusz Holko. All rights reserved.
@@ -10,7 +10,7 @@ import UIKit
 import CoreGraphics
 import QuartzCore
 
-class BendingLayer: CALayer {
+class BendableLayer: CALayer {
 
     override func addAnimation(anim: CAAnimation!, forKey key: String!) {
         super.addAnimation(anim, forKey: key)
@@ -24,7 +24,7 @@ class BendingLayer: CALayer {
     }
 }
 
-protocol BendingLayerDelegate {
+protocol BendableLayerDelegate {
     func positionAnimationWillStart(anim: CABasicAnimation)
 }
 
@@ -34,7 +34,7 @@ protocol BendingLayerDelegate {
 // to animate the change of the position and set `damping` and `initialSpringVelocity` to different values
 // than in that animation block. I propose to use slightly lower values for these properties.
 // These properties can't be set automatically, because `CASpringAnimation` is private.
-class BendingView: UIView, BendingLayerDelegate {
+class BendableView: UIView, BendableLayerDelegate {
 
     // MARK: Public properties
 
@@ -53,10 +53,10 @@ class BendingView: UIView, BendingLayerDelegate {
     // A hidden view that is used only for spring animation's simulation.
     // Its frame's origin matches the view's frame origin (except during animation). Of course it is in a different coordinate system,
     // but it doesn't matter to us. What we're interested in, is a position's difference between this subview's frame and the view's frame.
-    // This difference (`bendingOffset`) is used for bending the edges of the view.
+    // This difference (`BendableOffset`) is used for Bendable the edges of the view.
     let dummyView = UIView()
     let shapeLayer = CAShapeLayer()
-    var bendingOffset: UIOffset = UIOffsetZero {
+    var BendableOffset: UIOffset = UIOffsetZero {
     didSet {
         updatePath()
     }
@@ -87,7 +87,7 @@ class BendingView: UIView, BendingLayerDelegate {
     // MARK: UIView
 
     override class func layerClass() -> AnyClass {
-        return BendingLayer.self
+        return BendableLayer.self
     }
 
     override func layoutSubviews() {
@@ -97,7 +97,7 @@ class BendingView: UIView, BendingLayerDelegate {
         dummyView.frame.origin = frame.origin
     }
 
-    // MARK: BendingLayerDelegate
+    // MARK: BendableLayerDelegate
 
     func positionAnimationWillStart(anim: CABasicAnimation) {
         if !displayLink {
@@ -143,13 +143,13 @@ class BendingView: UIView, BendingLayerDelegate {
         let path = UIBezierPath()
         path.moveToPoint(CGPoint(x: 0, y: 0))
         path.addQuadCurveToPoint(CGPoint(x: width, y: 0),
-            controlPoint:CGPoint(x: width / 2.0, y: 0 + bendingOffset.vertical))
+            controlPoint:CGPoint(x: width / 2.0, y: 0 + BendableOffset.vertical))
         path.addQuadCurveToPoint(CGPoint(x: width, y: height),
-            controlPoint:CGPoint(x: width + bendingOffset.horizontal, y: height / 2.0))
+            controlPoint:CGPoint(x: width + BendableOffset.horizontal, y: height / 2.0))
         path.addQuadCurveToPoint(CGPoint(x: 0, y: height),
-            controlPoint: CGPoint(x: width / 2.0, y: height + bendingOffset.vertical))
+            controlPoint: CGPoint(x: width / 2.0, y: height + BendableOffset.vertical))
         path.addQuadCurveToPoint(CGPoint(x: 0, y: 0),
-            controlPoint: CGPoint(x: bendingOffset.horizontal, y: height / 2.0))
+            controlPoint: CGPoint(x: BendableOffset.horizontal, y: height / 2.0))
         path.closePath()
 
         shapeLayer.path = path.CGPath
@@ -162,7 +162,7 @@ class BendingView: UIView, BendingLayerDelegate {
     func tick(displayLink: CADisplayLink) {
         if let dummyViewPresentationLayer = dummyView.layer.presentationLayer() as? CALayer {
             if let presentationLayer = layer.presentationLayer() as? CALayer {
-                bendingOffset = UIOffset(horizontal: CGRectGetMinX(dummyViewPresentationLayer.frame) - CGRectGetMinX(presentationLayer.frame),
+                BendableOffset = UIOffset(horizontal: CGRectGetMinX(dummyViewPresentationLayer.frame) - CGRectGetMinX(presentationLayer.frame),
                     vertical: CGRectGetMinY(dummyViewPresentationLayer.frame) - CGRectGetMinY(presentationLayer.frame))
             }
         }

AHKBendableView.podspec
@@ -1,11 +1,11 @@
 Pod::Spec.new do |s|
-  s.name             = "AHKBendingView"
+  s.name             = "AHKBendableView"
   s.version          = "0.1.0"
   s.summary          = "UIView subclass that bends its edges when its position changes."
-  s.homepage         = "https://github.com/fastred/AHKBendingView"
+  s.homepage         = "https://github.com/fastred/AHKBendableView"
   s.license          = 'MIT'
   s.author           = { "Arkadiusz Holko" => "fastred@fastred.org" }
-  s.source           = { :git => "https://github.com/fastred/AHKBendingView.git", :tag => s.version.to_s }
+  s.source           = { :git => "https://github.com/fastred/AHKBendableView.git", :tag => s.version.to_s }
   s.social_media_url = 'https://twitter.com/arekholko'
   s.platform     = :ios, '7.0'
   s.requires_arc = true

Example/AHKBendableView.xcodeproj/project.pbxproj
@@ -11,8 +11,8 @@
 		A63AB7AF195F246E00546B00 /* ViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = A63AB7AE195F246E00546B00 /* ViewController.swift */; };
 		A63AB7B2195F246E00546B00 /* Main.storyboard in Resources */ = {isa = PBXBuildFile; fileRef = A63AB7B0195F246E00546B00 /* Main.storyboard */; };
 		A63AB7B4195F246E00546B00 /* Images.xcassets in Resources */ = {isa = PBXBuildFile; fileRef = A63AB7B3195F246E00546B00 /* Images.xcassets */; };
-		A63AB7C0195F246E00546B00 /* AHKBendingViewTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = A63AB7BF195F246E00546B00 /* AHKBendingViewTests.swift */; };
-		A65BE262195F24AB00B92356 /* BendingView.swift in Sources */ = {isa = PBXBuildFile; fileRef = A65BE261195F24AB00B92356 /* BendingView.swift */; };
+		A63AB7C0195F246E00546B00 /* AHKBendableViewTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = A63AB7BF195F246E00546B00 /* AHKBendableViewTests.swift */; };
+		A6F61CC3195F47C500AF4664 /* BendableView.swift in Sources */ = {isa = PBXBuildFile; fileRef = A6F61CC2195F47C500AF4664 /* BendableView.swift */; };
 /* End PBXBuildFile section */
 
 /* Begin PBXContainerItemProxy section */
@@ -21,21 +21,21 @@
 			containerPortal = A63AB79F195F246E00546B00 /* Project object */;
 			proxyType = 1;
 			remoteGlobalIDString = A63AB7A6195F246E00546B00;
-			remoteInfo = AHKBendingView;
+			remoteInfo = AHKBendableView;
 		};
 /* End PBXContainerItemProxy section */
 
 /* Begin PBXFileReference section */
-		A63AB7A7195F246E00546B00 /* AHKBendingView.app */ = {isa = PBXFileReference; explicitFileType = wrapper.application; includeInIndex = 0; path = AHKBendingView.app; sourceTree = BUILT_PRODUCTS_DIR; };
+		A63AB7A7195F246E00546B00 /* AHKBendableView.app */ = {isa = PBXFileReference; explicitFileType = wrapper.application; includeInIndex = 0; path = AHKBendableView.app; sourceTree = BUILT_PRODUCTS_DIR; };
 		A63AB7AB195F246E00546B00 /* Info.plist */ = {isa = PBXFileReference; lastKnownFileType = text.plist.xml; path = Info.plist; sourceTree = "<group>"; };
 		A63AB7AC195F246E00546B00 /* AppDelegate.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = AppDelegate.swift; sourceTree = "<group>"; };
 		A63AB7AE195F246E00546B00 /* ViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ViewController.swift; sourceTree = "<group>"; };
 		A63AB7B1195F246E00546B00 /* Base */ = {isa = PBXFileReference; lastKnownFileType = file.storyboard; name = Base; path = Base.lproj/Main.storyboard; sourceTree = "<group>"; };
 		A63AB7B3195F246E00546B00 /* Images.xcassets */ = {isa = PBXFileReference; lastKnownFileType = folder.assetcatalog; path = Images.xcassets; sourceTree = "<group>"; };
-		A63AB7B9195F246E00546B00 /* AHKBendingViewTests.xctest */ = {isa = PBXFileReference; explicitFileType = wrapper.cfbundle; includeInIndex = 0; path = AHKBendingViewTests.xctest; sourceTree = BUILT_PRODUCTS_DIR; };
+		A63AB7B9195F246E00546B00 /* AHKBendableViewTests.xctest */ = {isa = PBXFileReference; explicitFileType = wrapper.cfbundle; includeInIndex = 0; path = AHKBendableViewTests.xctest; sourceTree = BUILT_PRODUCTS_DIR; };
 		A63AB7BE195F246E00546B00 /* Info.plist */ = {isa = PBXFileReference; lastKnownFileType = text.plist.xml; path = Info.plist; sourceTree = "<group>"; };
-		A63AB7BF195F246E00546B00 /* AHKBendingViewTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = AHKBendingViewTests.swift; sourceTree = "<group>"; };
-		A65BE261195F24AB00B92356 /* BendingView.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = BendingView.swift; sourceTree = "<group>"; };
+		A63AB7BF195F246E00546B00 /* AHKBendableViewTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = AHKBendableViewTests.swift; sourceTree = "<group>"; };
+		A6F61CC2195F47C500AF4664 /* BendableView.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = BendableView.swift; sourceTree = "<group>"; };
 /* End PBXFileReference section */
 
 /* Begin PBXFrameworksBuildPhase section */
@@ -60,8 +60,8 @@
 			isa = PBXGroup;
 			children = (
 				A65BE260195F248F00B92356 /* Pod */,
-				A63AB7A9195F246E00546B00 /* AHKBendingView */,
-				A63AB7BC195F246E00546B00 /* AHKBendingViewTests */,
+				A63AB7A9195F246E00546B00 /* AHKBendableView */,
+				A63AB7BC195F246E00546B00 /* AHKBendableViewTests */,
 				A63AB7A8195F246E00546B00 /* Products */,
 			);
 			sourceTree = "<group>";
@@ -69,13 +69,13 @@
 		A63AB7A8195F246E00546B00 /* Products */ = {
 			isa = PBXGroup;
 			children = (
-				A63AB7A7195F246E00546B00 /* AHKBendingView.app */,
-				A63AB7B9195F246E00546B00 /* AHKBendingViewTests.xctest */,
+				A63AB7A7195F246E00546B00 /* AHKBendableView.app */,
+				A63AB7B9195F246E00546B00 /* AHKBendableViewTests.xctest */,
 			);
 			name = Products;
 			sourceTree = "<group>";
 		};
-		A63AB7A9195F246E00546B00 /* AHKBendingView */ = {
+		A63AB7A9195F246E00546B00 /* AHKBendableView */ = {
 			isa = PBXGroup;
 			children = (
 				A63AB7AC195F246E00546B00 /* AppDelegate.swift */,
@@ -84,7 +84,7 @@
 				A63AB7B3195F246E00546B00 /* Images.xcassets */,
 				A63AB7AA195F246E00546B00 /* Supporting Files */,
 			);
-			path = AHKBendingView;
+			path = AHKBendableView;
 			sourceTree = "<group>";
 		};
 		A63AB7AA195F246E00546B00 /* Supporting Files */ = {
@@ -95,13 +95,13 @@
 			name = "Supporting Files";
 			sourceTree = "<group>";
 		};
-		A63AB7BC195F246E00546B00 /* AHKBendingViewTests */ = {
+		A63AB7BC195F246E00546B00 /* AHKBendableViewTests */ = {
 			isa = PBXGroup;
 			children = (
-				A63AB7BF195F246E00546B00 /* AHKBendingViewTests.swift */,
+				A63AB7BF195F246E00546B00 /* AHKBendableViewTests.swift */,
 				A63AB7BD195F246E00546B00 /* Supporting Files */,
 			);
-			path = AHKBendingViewTests;
+			path = AHKBendableViewTests;
 			sourceTree = "<group>";
 		};
 		A63AB7BD195F246E00546B00 /* Supporting Files */ = {
@@ -115,7 +115,7 @@
 		A65BE260195F248F00B92356 /* Pod */ = {
 			isa = PBXGroup;
 			children = (
-				A65BE261195F24AB00B92356 /* BendingView.swift */,
+				A6F61CC2195F47C500AF4664 /* BendableView.swift */,
 			);
 			name = Pod;
 			path = ../Classes;
@@ -124,9 +124,9 @@
 /* End PBXGroup section */
 
 /* Begin PBXNativeTarget section */
-		A63AB7A6195F246E00546B00 /* AHKBendingView */ = {
+		A63AB7A6195F246E00546B00 /* AHKBendableView */ = {
 			isa = PBXNativeTarget;
-			buildConfigurationList = A63AB7C3195F246E00546B00 /* Build configuration list for PBXNativeTarget "AHKBendingView" */;
+			buildConfigurationList = A63AB7C3195F246E00546B00 /* Build configuration list for PBXNativeTarget "AHKBendableView" */;
 			buildPhases = (
 				A63AB7A3195F246E00546B00 /* Sources */,
 				A63AB7A4195F246E00546B00 /* Frameworks */,
@@ -136,14 +136,14 @@
 			);
 			dependencies = (
 			);
-			name = AHKBendingView;
-			productName = AHKBendingView;
-			productReference = A63AB7A7195F246E00546B00 /* AHKBendingView.app */;
+			name = AHKBendableView;
+			productName = AHKBendableView;
+			productReference = A63AB7A7195F246E00546B00 /* AHKBendableView.app */;
 			productType = "com.apple.product-type.application";
 		};
-		A63AB7B8195F246E00546B00 /* AHKBendingViewTests */ = {
+		A63AB7B8195F246E00546B00 /* AHKBendableViewTests */ = {
 			isa = PBXNativeTarget;
-			buildConfigurationList = A63AB7C6195F246E00546B00 /* Build configuration list for PBXNativeTarget "AHKBendingViewTests" */;
+			buildConfigurationList = A63AB7C6195F246E00546B00 /* Build configuration list for PBXNativeTarget "AHKBendableViewTests" */;
 			buildPhases = (
 				A63AB7B5195F246E00546B00 /* Sources */,
 				A63AB7B6195F246E00546B00 /* Frameworks */,
@@ -154,9 +154,9 @@
 			dependencies = (
 				A63AB7BB195F246E00546B00 /* PBXTargetDependency */,
 			);
-			name = AHKBendingViewTests;
-			productName = AHKBendingViewTests;
-			productReference = A63AB7B9195F246E00546B00 /* AHKBendingViewTests.xctest */;
+			name = AHKBendableViewTests;
+			productName = AHKBendableViewTests;
+			productReference = A63AB7B9195F246E00546B00 /* AHKBendableViewTests.xctest */;
 			productType = "com.apple.product-type.bundle.unit-test";
 		};
 /* End PBXNativeTarget section */
@@ -177,7 +177,7 @@
 					};
 				};
 			};
-			buildConfigurationList = A63AB7A2195F246E00546B00 /* Build configuration list for PBXProject "AHKBendingView" */;
+			buildConfigurationList = A63AB7A2195F246E00546B00 /* Build configuration list for PBXProject "AHKBendableView" */;
 			compatibilityVersion = "Xcode 3.2";
 			developmentRegion = English;
 			hasScannedForEncodings = 0;
@@ -190,8 +190,8 @@
 			projectDirPath = "";
 			projectRoot = "";
 			targets = (
-				A63AB7A6195F246E00546B00 /* AHKBendingView */,
-				A63AB7B8195F246E00546B00 /* AHKBendingViewTests */,
+				A63AB7A6195F246E00546B00 /* AHKBendableView */,
+				A63AB7B8195F246E00546B00 /* AHKBendableViewTests */,
 			);
 		};
 /* End PBXProject section */
@@ -220,8 +220,8 @@
 			isa = PBXSourcesBuildPhase;
 			buildActionMask = 2147483647;
 			files = (
-				A65BE262195F24AB00B92356 /* BendingView.swift in Sources */,
 				A63AB7AF195F246E00546B00 /* ViewController.swift in Sources */,
+				A6F61CC3195F47C500AF4664 /* BendableView.swift in Sources */,
 				A63AB7AD195F246E00546B00 /* AppDelegate.swift in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
@@ -230,7 +230,7 @@
 			isa = PBXSourcesBuildPhase;
 			buildActionMask = 2147483647;
 			files = (
-				A63AB7C0195F246E00546B00 /* AHKBendingViewTests.swift in Sources */,
+				A63AB7C0195F246E00546B00 /* AHKBendableViewTests.swift in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -239,7 +239,7 @@
 /* Begin PBXTargetDependency section */
 		A63AB7BB195F246E00546B00 /* PBXTargetDependency */ = {
 			isa = PBXTargetDependency;
-			target = A63AB7A6195F246E00546B00 /* AHKBendingView */;
+			target = A63AB7A6195F246E00546B00 /* AHKBendableView */;
 			targetProxy = A63AB7BA195F246E00546B00 /* PBXContainerItemProxy */;
 		};
 /* End PBXTargetDependency section */
@@ -338,7 +338,7 @@
 			buildSettings = {
 				ASSETCATALOG_COMPILER_APPICON_NAME = AppIcon;
 				ASSETCATALOG_COMPILER_LAUNCHIMAGE_NAME = LaunchImage;
-				INFOPLIST_FILE = AHKBendingView/Info.plist;
+				INFOPLIST_FILE = AHKBendableView/Info.plist;
 				IPHONEOS_DEPLOYMENT_TARGET = 7.1;
 				LD_RUNPATH_SEARCH_PATHS = "$(inherited) @executable_path/Frameworks";
 				PRODUCT_NAME = "$(TARGET_NAME)";
@@ -350,7 +350,7 @@
 			buildSettings = {
 				ASSETCATALOG_COMPILER_APPICON_NAME = AppIcon;
 				ASSETCATALOG_COMPILER_LAUNCHIMAGE_NAME = LaunchImage;
-				INFOPLIST_FILE = AHKBendingView/Info.plist;
+				INFOPLIST_FILE = AHKBendableView/Info.plist;
 				IPHONEOS_DEPLOYMENT_TARGET = 7.1;
 				LD_RUNPATH_SEARCH_PATHS = "$(inherited) @executable_path/Frameworks";
 				PRODUCT_NAME = "$(TARGET_NAME)";
@@ -360,7 +360,7 @@
 		A63AB7C7195F246E00546B00 /* Debug */ = {
 			isa = XCBuildConfiguration;
 			buildSettings = {
-				BUNDLE_LOADER = "$(BUILT_PRODUCTS_DIR)/AHKBendingView.app/AHKBendingView";
+				BUNDLE_LOADER = "$(BUILT_PRODUCTS_DIR)/AHKBendableView.app/AHKBendableView";
 				FRAMEWORK_SEARCH_PATHS = (
 					"$(SDKROOT)/Developer/Library/Frameworks",
 					"$(inherited)",
@@ -369,7 +369,7 @@
 					"DEBUG=1",
 					"$(inherited)",
 				);
-				INFOPLIST_FILE = AHKBendingViewTests/Info.plist;
+				INFOPLIST_FILE = AHKBendableViewTests/Info.plist;
 				LD_RUNPATH_SEARCH_PATHS = "$(inherited) @executable_path/Frameworks @loader_path/Frameworks";
 				METAL_ENABLE_DEBUG_INFO = YES;
 				PRODUCT_NAME = "$(TARGET_NAME)";
@@ -380,12 +380,12 @@
 		A63AB7C8195F246E00546B00 /* Release */ = {
 			isa = XCBuildConfiguration;
 			buildSettings = {
-				BUNDLE_LOADER = "$(BUILT_PRODUCTS_DIR)/AHKBendingView.app/AHKBendingView";
+				BUNDLE_LOADER = "$(BUILT_PRODUCTS_DIR)/AHKBendableView.app/AHKBendableView";
 				FRAMEWORK_SEARCH_PATHS = (
 					"$(SDKROOT)/Developer/Library/Frameworks",
 					"$(inherited)",
 				);
-				INFOPLIST_FILE = AHKBendingViewTests/Info.plist;
+				INFOPLIST_FILE = AHKBendableViewTests/Info.plist;
 				LD_RUNPATH_SEARCH_PATHS = "$(inherited) @executable_path/Frameworks @loader_path/Frameworks";
 				METAL_ENABLE_DEBUG_INFO = NO;
 				PRODUCT_NAME = "$(TARGET_NAME)";
@@ -396,7 +396,7 @@
 /* End XCBuildConfiguration section */
 
 /* Begin XCConfigurationList section */
-		A63AB7A2195F246E00546B00 /* Build configuration list for PBXProject "AHKBendingView" */ = {
+		A63AB7A2195F246E00546B00 /* Build configuration list for PBXProject "AHKBendableView" */ = {
 			isa = XCConfigurationList;
 			buildConfigurations = (
 				A63AB7C1195F246E00546B00 /* Debug */,
@@ -405,7 +405,7 @@
 			defaultConfigurationIsVisible = 0;
 			defaultConfigurationName = Release;
 		};
-		A63AB7C3195F246E00546B00 /* Build configuration list for PBXNativeTarget "AHKBendingView" */ = {
+		A63AB7C3195F246E00546B00 /* Build configuration list for PBXNativeTarget "AHKBendableView" */ = {
 			isa = XCConfigurationList;
 			buildConfigurations = (
 				A63AB7C4195F246E00546B00 /* Debug */,
@@ -414,7 +414,7 @@
 			defaultConfigurationIsVisible = 0;
 			defaultConfigurationName = Release;
 		};
-		A63AB7C6195F246E00546B00 /* Build configuration list for PBXNativeTarget "AHKBendingViewTests" */ = {
+		A63AB7C6195F246E00546B00 /* Build configuration list for PBXNativeTarget "AHKBendableViewTests" */ = {
 			isa = XCConfigurationList;
 			buildConfigurations = (
 				A63AB7C7195F246E00546B00 /* Debug */,

Example/AHKBendableView.xcodeproj/project.xcworkspace/contents.xcworkspacedata
@@ -2,6 +2,6 @@
 <Workspace
    version = "1.0">
    <FileRef
-      location = "self:AHKBendingView.xcodeproj">
+      location = "self:AHKBendableView.xcodeproj">
    </FileRef>
 </Workspace>

Example/AHKBendableView/AppDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.swift
-//  AHKBendingView
+//  AHKBendableView
 //
 //  Created by Arkadiusz on 28-06-14.
 //  Copyright (c) 2014 Arkadiusz Holko. All rights reserved.

Example/AHKBendableView/Base.lproj/Main.storyboard
@@ -7,7 +7,7 @@
         <!--View Controller-->
         <scene sceneID="ufC-wZ-h7g">
             <objects>
-                <viewController id="vXZ-lx-hvc" customClass="ViewController" customModule="AHKBendingView" customModuleProvider="target" sceneMemberID="viewController">
+                <viewController id="vXZ-lx-hvc" customClass="ViewController" customModule="AHKBendableView" customModuleProvider="target" sceneMemberID="viewController">
                     <layoutGuides>
                         <viewControllerLayoutGuide type="top" id="jyV-Pf-zRb"/>
                         <viewControllerLayoutGuide type="bottom" id="2fi-mo-0CV"/>
@@ -16,7 +16,7 @@
                         <rect key="frame" x="0.0" y="0.0" width="480" height="480"/>
                         <autoresizingMask key="autoresizingMask" flexibleMaxX="YES" flexibleMaxY="YES"/>
                         <subviews>
-                            <view contentMode="scaleToFill" translatesAutoresizingMaskIntoConstraints="NO" id="rl2-17-HkY" customClass="BendingView" customModule="AHKBendingView" customModuleProvider="target">
+                            <view contentMode="scaleToFill" translatesAutoresizingMaskIntoConstraints="NO" id="rl2-17-HkY" customClass="BendableView" customModule="AHKBendableView" customModuleProvider="target">
                                 <rect key="frame" x="0.0" y="20" width="100" height="100"/>
                                 <subviews>
                                     <label opaque="NO" clipsSubviews="YES" userInteractionEnabled="NO" contentMode="left" horizontalHuggingPriority="251" verticalHuggingPriority="251" text="Test" lineBreakMode="tailTruncation" baselineAdjustment="alignBaselines" adjustsFontSizeToFit="NO" translatesAutoresizingMaskIntoConstraints="NO" id="STI-vD-iWl">
@@ -67,7 +67,7 @@
                         <simulatedOrientationMetrics key="simulatedOrientationMetrics" orientation="landscapeRight"/>
                     </view>
                     <connections>
-                        <outlet property="bendingView" destination="rl2-17-HkY" id="LsO-fv-FhA"/>
+                        <outlet property="BendableView" destination="rl2-17-HkY" id="LsO-fv-FhA"/>
                         <outlet property="heightConstraint" destination="M0S-h7-JZd" id="GLT-lc-kLw"/>
                         <outlet property="leadingConstraint" destination="y8N-kY-ARU" id="dQ1-af-MHb"/>
                         <outlet property="topConstraint" destination="iwV-VE-Zh4" id="lYD-Ph-fw6"/>

Example/AHKBendableView/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  AHKBendingView
+//  AHKBendableView
 //
 //  Created by Arkadiusz on 26-06-14.
 //  Copyright (c) 2014 Arkadiusz Holko. All rights reserved.
@@ -11,7 +11,7 @@ import QuartzCore
 
 class ViewController: UIViewController {
 
-    @IBOutlet var bendingView: BendingView
+    @IBOutlet var bendableView: BendableView
     @IBOutlet var topConstraint: NSLayoutConstraint
     @IBOutlet var leadingConstraint: NSLayoutConstraint
     @IBOutlet var widthConstraint: NSLayoutConstraint
@@ -27,8 +27,8 @@ class ViewController: UIViewController {
 
     func randomizePosition(position: Bool, randomizeSize: Bool) {
 
-        bendingView.damping = 0.7
-        bendingView.initialSpringVelocity = 0.8
+        bendableView.damping = 0.7
+        bendableView.initialSpringVelocity = 0.8
 
         if (randomizeSize) {
             widthConstraint.constant = CGFloat(arc4random() % 50) + 150

commit add66e485d903a1b5dbd8e1101a08705139f791e

Fix problems caused by c9f3b1b94e3894fc13f1034a2412e85923173a55

Classes/BendableView.swift
@@ -52,10 +52,10 @@ class BendableView: UIView, BendableLayerDelegate {
     // A hidden view that is used only for spring animation's simulation.
     // Its frame's origin matches the view's frame origin (except during animation). Of course it is in a different coordinate system,
     // but it doesn't matter to us. What we're interested in, is a position's difference between this subview's frame and the view's frame.
-    // This difference (`BendableOffset`) is used for Bendable the edges of the view.
+    // This difference (`bendableOffset`) is used for bending the edges of the view.
     let dummyView = UIView()
     let shapeLayer = CAShapeLayer()
-    var BendableOffset: UIOffset = UIOffsetZero {
+    var bendableOffset: UIOffset = UIOffsetZero {
     didSet {
         updatePath()
     }
@@ -142,13 +142,13 @@ class BendableView: UIView, BendableLayerDelegate {
         let path = UIBezierPath()
         path.moveToPoint(CGPoint(x: 0, y: 0))
         path.addQuadCurveToPoint(CGPoint(x: width, y: 0),
-            controlPoint:CGPoint(x: width / 2.0, y: 0 + BendableOffset.vertical))
+            controlPoint:CGPoint(x: width / 2.0, y: 0 + bendableOffset.vertical))
         path.addQuadCurveToPoint(CGPoint(x: width, y: height),
-            controlPoint:CGPoint(x: width + BendableOffset.horizontal, y: height / 2.0))
+            controlPoint:CGPoint(x: width + bendableOffset.horizontal, y: height / 2.0))
         path.addQuadCurveToPoint(CGPoint(x: 0, y: height),
-            controlPoint: CGPoint(x: width / 2.0, y: height + BendableOffset.vertical))
+            controlPoint: CGPoint(x: width / 2.0, y: height + bendableOffset.vertical))
         path.addQuadCurveToPoint(CGPoint(x: 0, y: 0),
-            controlPoint: CGPoint(x: BendableOffset.horizontal, y: height / 2.0))
+            controlPoint: CGPoint(x: bendableOffset.horizontal, y: height / 2.0))
         path.closePath()
 
         shapeLayer.path = path.CGPath
@@ -161,7 +161,7 @@ class BendableView: UIView, BendableLayerDelegate {
     func tick(displayLink: CADisplayLink) {
         if let dummyViewPresentationLayer = dummyView.layer.presentationLayer() as? CALayer {
             if let presentationLayer = layer.presentationLayer() as? CALayer {
-                BendableOffset = UIOffset(horizontal: CGRectGetMinX(dummyViewPresentationLayer.frame) - CGRectGetMinX(presentationLayer.frame),
+                bendableOffset = UIOffset(horizontal: CGRectGetMinX(dummyViewPresentationLayer.frame) - CGRectGetMinX(presentationLayer.frame),
                     vertical: CGRectGetMinY(dummyViewPresentationLayer.frame) - CGRectGetMinY(presentationLayer.frame))
             }
         }